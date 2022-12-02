6e7405bba5a4784cac0c3247f9bfc11e9f972d0b
your name <your email>
2022-12-02 17:23:49 +0000

    ready to be reviewed

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,10 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    special = password.include?('!') || password.include?('@') || password.include?('$') || password.include?('%') || password.include?('&')
+    if password.length > 7 && special
+        return true
+    else
+        return false
+    end
 end
